842f44c682f3c8056ff8e91d81d8a8c18a84bfde
takwa ibrahim <104450932+takwaibrahimmohamed@users.noreply.github.com>
2023-04-28 17:09:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+const allskekton = document.querySelectorAll(".bg-preload")
+function load(){
+allskekton.forEach((item)=>{
+item.classList.remove('bg-preload')
+})
+}
+
+setTimeout(() => {
+    load()
+}, 2000);
+
+
+
+// filterfunction
+
+const filters = document.querySelectorAll('.filter');
+
+
+filters.forEach(filter => { 
+    
+  filter.addEventListener('click', function() {
+
+    let selectedFilter = filter.getAttribute('data-filter');
+    let itemsToHide = document.querySelectorAll(`.projects .project:not([data-filter='${selectedFilter}'])`);
+    let itemsToShow = document.querySelectorAll(`.projects [data-filter='${selectedFilter}']`);
+    if (selectedFilter == 'all') {
+        itemsToHide = [];
+    
+      itemsToShow = document.querySelectorAll('.projects [data-filter]');
+     
+    }
+    
+    itemsToHide.forEach(el => {
+       
+      el.classList.add('hide');
+      el.classList.remove('show');
+    });
+
+    itemsToShow.forEach(el => {
+      el.classList.remove('hide');
+      el.classList.add('show'); 
+    });
+
+  });
+});
+
+///////////////
+
